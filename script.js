640ac3d97ef835c613254b4b859a1cd3be701357
hyuuwrdani-lgtm <hyuuwrdani@gmail.com>
2025-10-23 10:27:07 +0700

    Create script.js
    Signed-off-by: hyuuwrdani-lgtm <hyuuwrdani@gmail.com>

---
+++ script.js
@@ -0,0 +1,6 @@
+// 1. pilih tombol berdasarkan id-nya
+cost tombolSapa = document.getelemntById('sapaButton');
+// 2. Tambahkan 'event listener' untuk aksi 'clik'
+tombolSapa.addEvventListener('clik', function() {
+  // 3. jalankan kode ini ketika tombol di-klik
+    alert('halo! Terima Kasih sudah Berkunjung!)
